57a393a741b6b99598c595e6e61a64906d7c1311
Paola <paola24539@cbtis75.edu.mx>
2025-10-03 17:54:14 -0600

    Pao

---
+++ proyecto/clases/bebida.py
@@ -0,0 +1,25 @@
+class Bebida:
+    def __init__(self, nombre, tipo, tamaño, temperatura, precio):
+        self.nombre = nombre
+        self.tipo = tipo
+        self.tamaño = tamaño
+        self.temperatura = temperatura
+        self.precio = precio
+    
+    def mostrar(self):
+        return f"Bebida: {self.nombre}"
+        return f"Tipo: {self.tipo}" 
+        return f"Tamaño: {self.tamaño}"
+        return f"Temperatura: {self.temperatura}"
+        return f"Precio: ${self.precio}"
+    
+    def prepara(self):
+        return f"Preparado tu {self.nombre}, {self.temperatura} en tamaño {self.tamaño}... ¡TU {self.nombre} ESTA LISTO!"
+    
+    def tomar(self):
+        return f"Estas tomando {self.nombre}...¡Disfuta!"
+    
+    def aplicar_descuento(self, porcentaje):
+        descuento = self.precio * (porcentaje/100)
+        self.precio -= descuento
+        return f"Se aplico un {porcentaje}% de descuento. Nuevo precio: ${self.precio: .2f}"
