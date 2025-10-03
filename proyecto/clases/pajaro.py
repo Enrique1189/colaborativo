2192faa29e03f97e99bdd56584ee527fe5999878
Wero <miguel24527@cbtis75.edu.mx>
2025-10-03 17:49:34 -0600

    pajaro

---
+++ proyecto/clases/pajaro.py
@@ -0,0 +1,7 @@
+class Pajaro:
+    def __init__(self, nombre, tipo, peso, altura, sintomas):
+        self.nombre = nombre
+        self.tipo = tipo
+        self.peso = peso
+        self.altura = altura
+        self.sintomas = sintomas
